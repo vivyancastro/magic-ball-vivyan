b43918b6975086df033b92fd63f3f5656ca541b3
Vivyan Rodrigues <vivyan.castro@gmail.com>
2021-04-05 15:59:14 -0300

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+      MaterialApp(home: MagicBallPage()),
+    );
+
+class MagicBallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue[100],
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.lightBlue[200],
+      ),
+      body: MagicBall(),
+    );
+  }
+}
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int answer = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            answer = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$answer.png'),
       ),
     );
+  }
+}
